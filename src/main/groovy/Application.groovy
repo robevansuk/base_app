726e68d777f7a6ee31acd3c1974b64ec905c0ab4
robevans <robevansuk@users.noreply.github.com>
2020-11-11 14:38:02 +0000

    Including a basic test with various JUnit5 compatible annotations

--- src/main/groovy/Application.groovy
+++ src/main/groovy/Application.groovy
@@ -1,12 +1,13 @@
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
 
 /**
  * Created on 11/22/16.
  */
 class Application {
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
   }
 }
--- src/test/groovy/SomeClassSpec.groovy
+++ src/test/groovy/SomeClassSpec.groovy
@@ -1,8 +1,21 @@
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.support.AnnotationConfigContextLoader
 import spock.lang.Specification
 
 /**
  * Created on 11/22/16.
  */
+@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 class SomeClassSpec extends Specification {
 
+    def "should check this is not null"() {
+        expect: "this is not null"
+        this != null
+    }
 }
